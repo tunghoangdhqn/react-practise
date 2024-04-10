2bd89d2aee1e755bb05eb4988dbc91612df76df5
Tung Hoang <tufnghd@gmail.com>
2024-04-10 21:51:52 +0700

    edit

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'; // nạp thư viện react
-import ReactDOM from 'react-dom'; // nạp thư viện react-dom
+import ReactDOM from 'react-dom/client'; // nạp thư viện react-dom
 
 // Tạo component App
 function App() {
     return (
         <div>
             <h1>Xin chào anh em F8!</h1>
-            <h2>acd</h2>
+            <h2>acdc</h2>
         </div>
     );
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById('root'));
+// react 2017
+//ReactDOM.render(<App />, document.getElementById('root'));
