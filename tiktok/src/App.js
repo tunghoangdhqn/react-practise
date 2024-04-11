b3517614214c68a2c04fe08a3ec26c6abd861fc9
Tung Hoang Laptop P15 <tufnghd@gmail.com>
2024-04-11 16:23:12 +0700

    use state

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+
+const order = [100, 200, 300];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState(() => {
+        const total = order.reduce((total, current) => { return total + current; });
+        return total;
+    });
+
+    const [info, setInfo] = useState(
+        {
+            name: 'Nguyen van a',
+            age: 18,
+            address: 'Ha noi'
+        }
+    );
+
+    const handleIncreate = () => {
+
+        // function trả về của useState, gọi lại hàm App khi chạy (bỏ giá trị khởi tại)
+        //setCounter(counter + 1);
+
+        //prevState: giá trị trước của state
+        setCounter(prevState => prevState + 1);
+    };
+    const handleUpdateInfo = () => {
+        setInfo({
+            ...info,
+            bio: 'nam'
+        });
+
+    };
+
+    return (
+        <div className="App">
+            <h1>{counter}</h1>
+            <button onClick={handleIncreate}>Increate</button>
+            <h1>{JSON.stringify(info)}</h1>
+            <button onClick={handleUpdateInfo}>Update Info</button>
+        </div>
+    );
 }
 
 export default App;
