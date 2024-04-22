86f34683677cdacbe97a96f4926aefa6331d061f
Tung Hoang Laptop P15 <tufnghd@gmail.com>
2024-04-22 15:03:28 +0700

    learn 3

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -5,15 +5,10 @@ import { useEffect } from 'react';
 import Content from './Content';
 
 function App() {
-  const [showContent, setShowContent] = useState(() => {
-    console.log('mounted');
-    return false;
-  });
 
   return (
     <div>
-      <button onClick={() => { setShowContent(!showContent); }}>Toggle content</button>
-      {showContent && <Content />}
+
     </div>
   );
 }
---
+++ tiktok/src/App_useEffect.js
@@ -0,0 +1,20 @@
+
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import Content from './Content';
+
+function App() {
+  const [showContent, setShowContent] = useState(() => {
+    console.log('mounted');
+    return false;
+  });
+
+  return (
+    <div>
+      <button onClick={() => { setShowContent(!showContent); }}>Toggle content</button>
+      {showContent && <Content />}
+    </div>
+  );
+}
+export default App;
