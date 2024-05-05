455d1791a8977c28d521385efe4e0d523ade82eb
Tung Hoang <tufnghd@gmail.com>
2024-05-05 22:25:33 +0700

    cm

--- tiktok-ui/src/App.js
+++ tiktok-ui/src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            const Layout = route.layout === null ? Fragment : DefaultLayout;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
