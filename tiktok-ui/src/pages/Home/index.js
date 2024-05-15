afb14302835df13fec493571a2ff6b7b4ec4d059
Tung Hoang <tufnghd@gmail.com>
2024-05-15 23:32:58 +0700

    axios

--- tiktok-ui/src/pages/Home/index.js
+++ tiktok-ui/src/pages/Home/index.js
@@ -1,4 +1,18 @@
+import axios from 'axios';
 function Home() {
+  axios
+    .get(`https://tiktok.fullstack.edu.vn/api/users/search`, {
+      params: {
+        q: 'hoa',
+        type: 'less',
+      },
+    })
+    .then((res) => {
+      console.log(res.data.data);
+    })
+    .catch(() => {
+      console.log('catch');
+    });
   return <h2>Home page</h2>;
 }
 
