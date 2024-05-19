45b4fb00000b3d7e69fd7e4c087a1b16aa833780
Tung Hoang <tufnghd@gmail.com>
2024-05-19 23:21:10 +0700

    learn 2

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import Post from "./components/Post";
-
 function App() {
+  const [todos, setTodos] = useState([{ name: "lau nha" }, { name: "lau nha 2" }]);
+  const [inputState, setInputState] = useState("");
+
+  const handleAddTodo = () => {
+    setTodos([...todos, { name: inputState }]);
+  };
+
+  const handleDeleteTodo = (index) => {
+    //console.log(todos.slice(index));
+    console.log(index);
+    setTodos(todos.slice(index + 1));
+  };
   return (
     <div>
-      <Post author="name" body="name2" />
+      <div>
+        <input
+          onChange={(e) => {
+            setInputState(e.target.value);
+          }}
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      {todos.map((value, index) => {
+        return (
+          <div key={index}>
+            <h2>{value.name}</h2>
+            <span onClick={() => handleDeleteTodo(index)}>X</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
--- tiktok/src/index.js
+++ tiktok/src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { StoreProvider } from './GlobalState/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StoreProvider } from "./GlobalState/store";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-//console.log(React);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
